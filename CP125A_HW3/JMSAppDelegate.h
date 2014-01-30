7b2448b70209d0976959fc8530027cdb40a96bdd
Jared <jared@jsorge.net>
2014-01-29 19:25:10 -0800

    Initial Commit

---
+++ CP125A_HW3/JMSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMSAppDelegate.h
+//  CP125A_HW3
+//
+//  Created by Jared Sorge on 1/29/14.
+//  Copyright (c) 2014 jsorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CP125A_HW3/JMSViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMSViewController.h
+//  CP125A_HW3
+//
+//  Created by Jared Sorge on 1/29/14.
+//  Copyright (c) 2014 jsorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMSViewController : UIViewController
+
+@end
